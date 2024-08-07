1a632d81836e9c66756d05661107e217c5118fcb
jparas-3 <jrsyparas@gmail.com>
2024-08-07 00:01:30 -0400

    added main.h

---
+++ include/main.h
@@ -0,0 +1,14 @@
+//
+// Created by jrsyp on 8/7/2024.
+//
+
+#ifndef LN_CORELOOP_MAIN_H
+#define LN_CORELOOP_MAIN_H
+
+enum cmd_format {
+    UNSPECIFIED,
+    CMD_FILE,
+    CMD_PORT,
+};
+
+#endif //LN_CORELOOP_MAIN_H
